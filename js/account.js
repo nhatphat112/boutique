bf5f482770af3cab445d8cd6e0297d021e6a8f8e
xiucaliu <lelinh050896@gmail.com>
2023-08-08 23:04:38 +0700

    account_change_password

--- js/account.js
+++ js/account.js
@@ -1,33 +1,37 @@
-// $(document).ready(function() {
-//     $("#profile").show();
-
-//     // $("#edit-profile-link").click(function(event) {
-//     //     event.preventDefault();
-//     //     $("#profile").hide();
-//     //     $("#edit-profile").show();
-
-//     // })
-//     // $("#edit-profile-link").click(function(event) {
-//     //     event.preventDefault();
-//     //     $("#profile").hide();
-//     //     $("#edit-profile").show();
-
-//     // })
-//     // change-password
-
-//     $(".display-page").click(function(event) {
-//         $(this)
-//     })
-
-
-// })
-
 $(document).ready(function() {
     $("#profile").show();
+    // Ẩn hiện các trang khi bấm vào các đề mục
     $('.toggle-link').click(function(event) {
         event.preventDefault();
         var target = $(this).data('target');
         $('.toggle-page').hide();
         $('#' + target).show();
     });
+    // Kết thúc ẩn hiện các trang khi bấm vào các đề mục
+
+    //Bắt đầu thay đổi mật khẩu
+    $('#change-pass-submit').click(function() {
+        var id = localStorage.getItem("userId");
+        console.log(id);
+        var currentPass = $('#currentPass').val();
+        var newPass = $('#newPass').val();
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/user/changepass",
+            async: false,
+            data: {
+                id: id,
+                currentPass: currentPass,
+                newPass: newPass
+            },
+            success: function(response) {
+                console.log(response.data);
+            },
+            error: function(error) {
+                console.error("change pass error", error);
+            }
+
+        })
+        console.log('helllo')
+    })
 });
